317e4eaea62c8eecc928f904f59d28e7e78158d4
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-06-11 19:47:54 -0700

    feat: add redis caching to redis server

--- utils/redis.js
+++ utils/redis.js
@@ -1,18 +1,50 @@
-import {createClient } from 'redis'
-const client = createClient()
+import { createClient } from 'redis';
 
+const client = createClient({
+    username: 'default',
+    password: process.env.REDIS_PASSWORD,
+    socket: {
+        host: 'redis-17648.c114.us-east-1-4.ec2.redns.redis-cloud.com',
+        port: 17648
+    }
+});
 
-// start connection to redis server
+client.on('error', err => console.log('Redis Client Error', err));
+
 const startRedis = async () =>{
     try{
-    await client.connect()
-    console.log('Successfully connected to Redis server')
-    }catch(error){
-        client.on('error', err => console.log(`Unable to connect to Redis server!  \n${err}`))
+        await client.connect();
+        console.log('Successfully connected to redis server')  
+    }catch(err){
+        console.log('Redis connection failed!', err)
     }
 }
 
-//start connection here
 startRedis()
-
 export default client
+
+
+
+
+
+
+
+
+// import {createClient } from 'redis'
+// const client = createClient()
+
+
+// // start connection to redis server
+// const startRedis = async () =>{
+//     try{
+//     await client.connect()
+//     console.log('Successfully connected to Redis server')
+//     }catch(error){
+//         client.on('error', err => console.log(`Unable to connect to Redis server!  \n${err}`))
+//     }
+// }
+
+// //start connection here
+// startRedis()
+
+// export default client
