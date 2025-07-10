37683598a3f65238b4ec61ff3174958dda10a3b3
Melrose <198425854+cobraxcodes@users.noreply.github.com>
2025-07-09 18:34:07 -0700

    fix: add rank and fix areacode

--- models/model.js
+++ models/model.js
@@ -3,8 +3,9 @@ import mongoose from "mongoose"
 const applicationSchema = new mongoose.Schema({
     name: {type: String, required: true},
     applicationDate : {type: Date, required: true},
+    areaCode: {type: Number, required: true},
     rank: {type: Number, required: true},
-    zipCode: {type: String}
+    zipcode: {type: String}
 
 })
 
